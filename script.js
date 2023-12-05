99ae06256bf8a64fc3e87950a02400bf38b343f0
Lawrence Luo <luolawrence1@gmail.com>
2023-12-04 20:38:45 -0800

    autoplaying with sound done

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+START_TIME = 0;
 VIDEO_LINKS = [];
 SWIPE_THRESHOLD = 3;  // How many swipes allowed per second
 SWIPES = 0;  // How many swipes have been made
 
+function canSwipe() {
+	let currentTime = new Date().getTime();
+	let elapsedTime = (currentTime - START_TIME) / 1000;
+	console.log(elapsedTime)
+	let swipesPerSecond = SWIPES / elapsedTime;
+	console.log("swipes per second: " + swipesPerSecond);
+
+	if (swipesPerSecond > (SWIPE_THRESHOLD * 1000)) {
+		console.log("too many swipes");
+		return false;
+	}
+
+	return true;
+}
+
 function beginExperiment() {
+	START_TIME = new Date().getTime();
+
 	// Hide the start button
 	let startButton = document.getElementById("startContainer");
 	startButton.style.display = "none";
@@ -18,7 +36,20 @@ function beginExperiment() {
 	// renderVideos(videoLinks);
 
 	// Play the first video
-	playVideo(0);
+	// playVideo(0);
+
+	// basically, videos aren't allowed to autoplay with sound if there is no explicit action from	the user
+	// swiping is not considered an explicit action
+	// The only explicit action we have is clicking the start button at the beginning.
+	// Therefore, when the user presses the start button, we need to "pre-play" all the videos
+	// so that they are ready to play when we swipe to them.
+	// The videos not on the page are paused, but with autoplay and sound activated
+	// When we do swipe to them, playVideo() is called and they are allowed to autoplay with sound
+	var iframes = document.querySelectorAll('iframe');
+	for (let i = 0; i < iframes.length; i++) {
+		var iframe = iframes[i];
+		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'playVideo' }), '*');
+	}
 }
 
 function stopExperiment() {
@@ -38,13 +69,13 @@ function stopExperiment() {
 	console.log("swipes: " + SWIPES);
 }
 
-function renderVideos(videoLinks) {
+function renderVideos(videoLinks, isProcessed=false) {
 	// <div class="content" style="transform: translateY(0px);"><iframe width="220" height="406" src="https://www.youtube.com/embed/9lQFQxvDnOc?enablejsapi=1&amp;loop=1&amp;playlist=9lQFQxvDnOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div>
 
 	let videosContainer = document.getElementById("container");
 	for (let i = 0; i < videoLinks.length; i++) {
 		let videoLink = videoLinks[i];
-		let newLink = convertLink(videoLink);
+		let newLink = isProcessed ? videoLink : convertLink(videoLink);
 		let newVideo = document.createElement("iframe");
 		newVideo.setAttribute("width", 220);
 		newVideo.setAttribute("height", 406);
@@ -66,7 +97,6 @@ function renderVideos(videoLinks) {
 		div.appendChild(newVideo);
 
 		videosContainer.appendChild(div);
-
 	}
 }
 
@@ -93,23 +123,27 @@ function stopVideo(index) {
 
 function playVideo(index) {
 	var iframes = document.querySelectorAll('iframe');
+
 	var iframe = iframes[index];
 	if (iframe)
 		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'playVideo' }), '*');
+		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'unMute' }), '*');
+
+		// setTimeout(function() {
+		//
+		// }, 1000);
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
 	function swipeTo(index) {
 		// guard against out of bounds index
-		if (index < 0 || index > maxIndex) return;
+		if (index < 0 || index > maxIndex);
 
 		currentContentIndex = index;
 		const offset = -index * window.innerHeight;
 		contents.forEach((content) => {
 				content.style.transform = `translateY(${offset}px)`;
 		});
-
-		SWIPES += 1;
 	}
 
 	const container = document.getElementById('container');
@@ -129,17 +163,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
 			let endY = e.changedTouches[0].clientY;
 			// swipe means 50px difference in beginning of drag and end of drag
 			if (startY - endY > 50) {
-					// Swipe Up
-					console.log("attempt swipe up from " + currentContentIndex + " to " + (currentContentIndex + 1));
+				// Swipe Up
+				console.log("attempt swipe up from " + currentContentIndex + " to " + (currentContentIndex + 1));
+
+				SWIPES += 1;
+				if (canSwipe()) {
 					stopVideo(currentContentIndex);
 					swipeTo(currentContentIndex + 1);
 					playVideo(currentContentIndex);  // play new video which is now at updated index?
+				}
 			} else if (endY - startY > 50) {
-					// Swipe Down
-					console.log("attempt swipe down from " + currentContentIndex + " to " + (currentContentIndex - 1));
+				// Swipe Down
+				SWIPES += 1;
+				console.log("attempt swipe down from " + currentContentIndex + " to " + (currentContentIndex - 1));
+
+				if (canSwipe()) {
 					stopVideo(currentContentIndex);
 					swipeTo(currentContentIndex - 1);
 					playVideo(currentContentIndex);  // play new video which is now at updated index?
+				}
 			}
 	});
 });
