c728070b772d364ac0f64d5e0430d1aa7bf89e09
Lawrence Luo <luolawrence1@gmail.com>
2024-10-22 17:00:18 -0700

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-START_TIME = 0;
-VIDEO_LINKS = [];
-SWIPE_THRESHOLD = 0.25; // 0.75;  // How many swipes allowed per second
+// Define global variables
+START_TIME = 0;  // replaced later when we call beginExperiment(): calls Date().getTime()
+SWIPE_THRESHOLD = 0.25;  // 0.75;  // How many swipes allowed per second
 SWIPES = 0;  // How many swipes have been made
 let csvData = "videoNumber,time\n";
 
@@ -57,22 +57,6 @@ function showToast() {
 	}, 1000);
 }
 
-function canSwipe() {
-	let currentTime = new Date().getTime();
-	let elapsedTime = (currentTime - START_TIME) / 1000;
-	console.log("elapsed time: " + elapsedTime);
-	let swipesPerSecond = SWIPES / elapsedTime;
-	console.log("swipes per second: " + swipesPerSecond);
-
-	if (swipesPerSecond > (SWIPE_THRESHOLD)) {
-		showToast();
-		return false;
-	} else {
-		addData(SWIPES, elapsedTime);
-		return true;
-	}
-}
-
 function beginExperiment() {
 	START_TIME = new Date().getTime();
 
@@ -80,6 +64,7 @@ function beginExperiment() {
 	let startButton = document.getElementById("startContainer");
 	startButton.style.display = "none";
 
+	// Show the stop button
 	let stopButton = document.getElementById("stopButton");
 	stopButton.style.display = "block";
 
@@ -87,28 +72,25 @@ function beginExperiment() {
 	let videosContainer = document.getElementById("container");
 	videosContainer.style.display = "block";
 
-	// Render the videos
-	// renderVideos(videoLinks);
-
-	// Play the first video
-
-	// basically, videos aren't allowed to autoplay with sound if there is no explicit action from	the user
-	// swiping is not considered an explicit action
+	// basically, videos aren't allowed to autoplay with sound if there is no explicit action from the user
+	// swiping is not considered an explicit action/consent from the user to play a video with sound
 	// The only explicit action we have is clicking the start button at the beginning.
+
 	// Therefore, when the user presses the start button, we need to "pre-play" all the videos
 	// so that they are ready to play when we swipe to them.
-	// The videos not on the page are paused, but with autoplay and sound activated
+	// The videos that are not currently on the page are paused, but with autoplay and sound activated
 	// When we do swipe to them, playVideo() is called and they are allowed to autoplay with sound
 	var iframes = document.querySelectorAll('iframe');
 	for (let i = 0; i < iframes.length; i++) {
 		var iframe = iframes[i];
 		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'playVideo' }), '*');
-		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');  // immediately stop videos but leave them in a preloaded state where they can autoplay unmuted
+
+		// immediately stop videos but leave them in a preloaded state where they can autoplay unmuted
+		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');
 	}
 
 	// replay first video to unmute it and ensure that it plays first
 	playVideo(0);
-
 }
 
 function endExperiment() {
@@ -130,13 +112,18 @@ function endExperiment() {
 }
 
 function renderVideos(videoLinks, isProcessed=false) {
-	// <div class="content" style="transform: translateY(0px);"><iframe width="220" height="406" src="https://www.youtube.com/embed/9lQFQxvDnOc?enablejsapi=1&amp;loop=1&amp;playlist=9lQFQxvDnOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe></div>
+	/*
+		<div class="content" style="transform: translateY(0px);">
+			<iframe width="220" height="406" src="https://www.youtube.com/embed/9lQFQxvDnOc?enablejsapi=1&amp;loop=1&amp;playlist=9lQFQxvDnOc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+		</div>
+	*/
 
 	let videosContainer = document.getElementById("container");
 	for (let i = 0; i < videoLinks.length; i++) {
 		let videoLink = videoLinks[i];
 		let newLink = isProcessed ? videoLink : convertLink(videoLink);
 
+		// Create iframe using JS
 		let newVideo = document.createElement("iframe");
 		newVideo.setAttribute("width", "100%");
 		newVideo.setAttribute("height", window.innerHeight * 0.75);
@@ -145,15 +132,17 @@ function renderVideos(videoLinks, isProcessed=false) {
 		newVideo.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
 		newVideo.setAttribute("allowfullscreen", "");
 
-    // Contain iframe and button inside div
+    // Create div to hold iframe, to help with positioning
 		let div = document.createElement("div");
 		div.setAttribute("class", "content");
 		div.appendChild(newVideo);
 
+		// Append the div that contains the iframe to the videosContainer
 		videosContainer.appendChild(div);
 	}
 }
 
+// Helper function to convert a youtube link into a working iframe link
 function convertLink(videoLink) {
 	// convert
 	// "https://www.youtube.com/embed/9lQFQxvDnOc"
@@ -175,27 +164,40 @@ function stopVideo(index) {
 		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');
 }
 
+// Selects the video at the given iframe index and plays it.
 function playVideo(index) {
 	var iframes = document.querySelectorAll('iframe');
-
 	var iframe = iframes[index];
 	if (iframe)
 		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'playVideo' }), '*');
 		iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'unMute' }), '*');
-
-		// setTimeout(function() {
-		//
-		// }, 1000);
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
+function canSwipe() {
+	let currentTime = new Date().getTime();
+	let elapsedTime = (currentTime - START_TIME) / 1000;
+	console.log("elapsed time: " + elapsedTime);
+	let swipesPerSecond = SWIPES / elapsedTime;
+	console.log("swipes per second: " + swipesPerSecond);
+
+	if (swipesPerSecond > SWIPE_THRESHOLD) {
+		showToast();  // show an unintrusive pop-up that says something like "too many swipes, please wait"
+		return false;
+	} else {
+		addData(SWIPES, elapsedTime);  // add a row of data for when this swipe occurred
+		return true;
+	}
+}
+
+// Main function: happens after HTML content loads
+document.addEventListener('DOMContentLoaded', () => {
 	const container = document.getElementById('container');
 	let currentContentIndex = 0;
-	const contents = document.querySelectorAll('.content');
+	const contents = document.querySelectorAll('.content');  // each div that contains an iframe
 	const maxIndex = contents.length - 1;
 
 	function swipeTo(index) {
-		// guard against out of bounds index
+		// guard against an index out of bounds error
 		if (index < 0 || index > maxIndex)
 			return;
 
@@ -210,7 +212,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	// screen height: 400
 	// vid2: start at 400-700
 	// vid3: start at 800-1100
-
 
 	// Basic swipe detection
 	let startY;
@@ -228,9 +229,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
 				console.log("attempt swipe up from " + currentContentIndex + " to " + (currentContentIndex + 1));
 
 				if (canSwipe()) {
-					stopVideo(currentContentIndex);
+					stopVideo(currentContentIndex);  // stop playing the current video
 					swipeTo(currentContentIndex + 1);
-					playVideo(currentContentIndex);  // play new video which is now at updated index?
+					playVideo(currentContentIndex);  // play new video which is now at updated index
 					SWIPES += 1;
 				}
 			} else if (endY - startY > 50) {
@@ -240,7 +241,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 				if (canSwipe()) {
 					stopVideo(currentContentIndex);
 					swipeTo(currentContentIndex - 1);
-					playVideo(currentContentIndex);  // play new video which is now at updated index?
+					playVideo(currentContentIndex);  // play new video which is now at updated index
 					SWIPES += 1;
 				}
 			}
